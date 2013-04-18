24b77b4c01f6f25ecdc4e923d29e7843489c8298
José Pedro Dias <jose.pedro.dias@gmail.com>
2013-04-18 17:14:30 +0100

    redone

---
+++ index.js
@@ -0,0 +1,150 @@
+var levelup = require('levelup'),
+    uuid    = require('node-uuid');
+
+
+
+var level1 = function(dbPath) {
+
+  // these are kept in closure scope
+  
+  var db = levelup(dbPath);
+
+
+
+  var now = function() {
+    return new Date().valueOf();
+  };
+
+
+
+  var parseIfPossible = function(val) {
+    try {
+      val = JSON.parse(val);
+    } catch (ex) {}
+    return val;
+  };
+
+
+
+  // these will get exported
+
+  var put = function(val, key) {
+    if (key === undefined) {
+      key = uuid.v1();
+    }
+
+    if (typeof val === 'object') {
+      var ts = now();
+      val._modifiedAt = ts;
+      if (!('_createdAt' in val)) {
+        val._createdAt = ts;
+      }
+
+      val = JSON.stringify(val);
+    }
+
+    db.put(key, val, function(err) {
+      if (err) { console.log('ERR:' + err.message); }
+    });
+
+    return key;
+  };
+
+
+
+  var get = function(key, cb) {
+    db.get(key, function(err, val) {
+      if (err) { return cb(err); }
+
+      val = parseIfPossible(val);
+
+      cb(null, val);
+    });
+  };
+
+
+
+  var keys = function(cb) {
+    var ks = db.createKeyStream();
+    var keys = [];
+    ks.on('data', function(k) {
+      keys.push(k);
+    });
+    ks.on('error', function(err) {
+      //ks.close();
+      cb(err);
+    });
+    ks.on('end', function() {
+      //ks.close();
+      cb(null, keys);
+    });
+  };
+
+
+
+  var values = function(cb) {
+    var res = [];
+    var rs = db.createReadStream();
+    rs.on('data', function(kv) {
+      res.push( parseIfPossible(kv.value) );
+    });
+    rs.on('error', function(err) {
+      //rs.close();
+      cb(err);
+    });
+    rs.on('end', function() {
+      //rs.close();
+      cb(null, res);
+    });
+  };
+
+
+
+  var search = function(filterFn, cb) {
+    var res = [];
+    var rs = db.createReadStream();
+    rs.on('data', function(kv) {
+      var v = parseIfPossible(kv.value);
+      var result = filterFn(kv.key, v);
+      if (result) {
+        res.push({key:kv.key, value:v});
+      }
+    });
+    rs.on('error', function(err) {
+      //rs.close();
+      cb(err);
+    });
+    rs.on('end', function() {
+      //rs.close();
+      cb(null, res);
+    });
+  };
+
+
+
+  var clear = function(cb) {
+    keys(function(err, keys) {
+      if (err) { return cb(err); }
+      keys.forEach(function(key) {  // TODO INCORRECT...
+        db.del(key);
+      });
+      cb(null);
+    });
+  };
+
+
+  return {
+    put:       put,
+    get:       get,
+    del:       function(key, cb) { return db.del(key, cb); },
+    keys:      keys,
+    values:    values,
+    search:    search,
+    list:      function(cb) { search(function() {return true;}, cb); },
+    clear:     clear
+  };
+};
+
+
+
+module.exports = level1;
---
+++ level1_cli.js
@@ -0,0 +1,145 @@
+var level1   = require('./index'), // or 'level1'
+    readline = require('readline');
+
+
+
+/**
+ * This is a command-line client that uses level1
+ *
+ * Syntax is:
+ * node level1_cli.js database_name
+ */
+
+
+
+var dbPath = process.argv.pop();
+if (dbPath.indexOf('/') === -1) {
+  dbPath = './' + dbPath;
+}
+
+var db = level1(dbPath);  // TODO CHECK DB EXISTS
+
+console.log('LOADING: ' + dbPath);
+
+
+
+var parseIfPossible = function(val) {
+  try {
+    val = JSON.parse(val);
+  } catch (ex) {}
+  return val;
+};
+
+
+
+var rl = readline.createInterface({
+  input:  process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(cmd) {
+  cmd = cmd.split(' ');
+  var op = cmd.shift();
+  var key;
+  switch (op) {
+    case 'add':
+      key = db.put( parseIfPossible( cmd.join(' ') ) );
+      console.log('data added to key ' + key + '\n');
+      break;
+
+    case 'put':
+      key = cmd.shift();
+      db.put( parseIfPossible( cmd.join(' ') ) , key);
+      console.log('data set on key ' + key + '\n');
+      break;
+
+    case 'get':
+      key = cmd.shift();
+      db.get(key, function(err, val) {
+        if (err) { return console.log(err.message+'\n'); }
+        console.log(JSON.stringify(val, null, '\t') + '\n');
+        //console.log(val+'\n');
+      });
+      break;
+
+    case 'del':
+      key = cmd.shift();
+      db.del(key, function(err) {
+        if (err) { return console.log(err.message+'\n'); }
+        console.log('deleted.\n');
+      });
+      break;
+
+    case 'list':
+    case 'search':
+      (function() {
+        var fn, fnBody = cmd.length ? cmd.join(' ') : 'return true;';
+
+        if (fnBody.indexOf('return') === -1) {
+          fnBody = 'return ' + fnBody;
+        }
+
+        try {
+          fn = new Function('k', 'v', fnBody);
+        } catch (err) {
+          return console.log('ERR:' + err);
+        }
+
+        db.search(fn, function(err, res) {
+          if (err) { return console.log(err.message+'\n'); }
+          var r = [];
+          res.forEach(function(kv) {
+            r = r.concat('* ', kv.key, ' -> ', JSON.stringify(kv.value, null, '\t'), '\n');
+          });
+          r = r.concat('TOTAL: ', res.length, '\n');
+          console.log(r.join(''));
+        });
+      })();
+      break;
+
+    case 'keys':
+      db.keys(function(err, keys) {
+        if (err) { return console.log('ERR:', err, '\n'); }
+        var r = [];
+        keys.forEach(function(key) {
+          r = r.concat('* ', key, '\n');
+        });
+        r = r.concat('TOTAL: ', keys.length, '\n');
+        console.log(r.join(''));
+      });
+      break;
+
+    case 'values':
+      db.values(function(err, values) {
+        if (err) { return console.log('ERR:', err, '\n'); }
+        var r = [];
+        values.forEach(function(val) {
+          r = r.concat(JSON.stringify(val, null, '\t'), '\n\n');
+        });
+        r = r.concat('TOTAL: ', values.length, '\n');
+        console.log(r.join(''));
+      });
+      break;
+
+    case 'clear':
+      db.clear(function(err) {
+        if (err) { return console.log('ERR:', err, '\n'); }
+        console.log('cleared.\n');
+      });
+      break;
+
+    default:
+      console.log([
+        'Supported operations are:\n',
+        '* add <data goes here>\n',
+        '* put <key> <data goes here>\n',
+        '* get <key>\n',
+        '* del <key>\n',
+        '* list\n',
+        '* search <function body, receiving arguments k, v and returning boolean>\n',
+        '* clear\n',
+        '* keys\n',
+        '* values\n\n'
+      ].join(''));
+  }
+});
